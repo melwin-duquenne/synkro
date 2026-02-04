803e6351acca95d4be0dcc1b127beb44d1ed0110
Kyllian Diochon <77027134+EkinOox@users.noreply.github.com>
2026-02-04 10:49:29 +0100

    Ajout d'une section pricing

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HeroSection from '@/components/landing/HeroSection.vue'
 import FeaturesSection from '@/components/landing/FeaturesSection.vue'
+import PricingSection from '@/components/landing/PricingSection.vue'
 import TeamSection from '@/components/landing/TeamSection.vue'
 import ChangelogSection from '@/components/landing/ChangelogSection.vue'
 import ContactSection from '@/components/landing/ContactSection.vue'
@@ -10,6 +11,7 @@ import ContactSection from '@/components/landing/ContactSection.vue'
   <div>
     <HeroSection />
     <FeaturesSection />
+    <PricingSection />
     <TeamSection />
     <ChangelogSection />
     <ContactSection />
